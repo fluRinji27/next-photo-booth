2c542f0548897b6929dab6154d8ec66b7917f851
fluRinji27 <denisov158742@gmail.com>
2021-07-14 20:04:09 +0300

    added sort method

--- pages/components/Catalog/Catalog.jsx
+++ pages/components/Catalog/Catalog.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Sort from '../Sort/Sort'
 import CatalogItem from './CatalogItem/CatalogItem'
 
@@ -6,20 +6,39 @@ import style from '/styles/Catalog/Catalog.module.scss'
 
 const Catalog = ({initialData}) => {
 
-    const [data, setData] = useState(initialData)
+    const [data, setData] = useState([])
 
     const setNewData = item => {
-        data.map(el => {
+        setData(data.map(el => {
             if (el.id === item.id) {
-
+                return item
             }
-        })
+            return el
+        }))
     }
+
+    const sortData = (type) => {
+        switch (type) {
+            case 'def':
+                setData(prev => [...prev.sort((a, b) => +a.id - +b.id)])
+                break;
+            case 'min':
+                setData(prev => [...prev.sort((a, b) => +a.price - +b.price).reverse()])
+                break;
+            case 'max':
+                setData(prev => [...prev.sort((a, b) => +a.price - +b.price)])
+                break
+        }
+    }
+
+    useEffect(() => {
+        setData(initialData)
+    }, [])
 
     return (
         <div className={'wrapper ' + style.catalog}>
             <h2 className="title title__h2 ">Фотобудки</h2>
-            <Sort/>
+            <Sort sortHandler={sortData}/>
             <ul>
                 {data && data.map(item => (
                     <CatalogItem key={item.id} setNewData={setNewData} data={item}/>
