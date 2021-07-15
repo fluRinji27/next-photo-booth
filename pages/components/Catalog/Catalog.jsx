1ac5ea6555a5dadeafc4984b35366c48740bed00
fluRinji27 <denisov158742@gmail.com>
2021-07-15 17:12:34 +0300

    remove unused scripts and change item height

--- pages/components/Catalog/Catalog.jsx
+++ pages/components/Catalog/Catalog.jsx
@@ -9,8 +9,6 @@ const Catalog = ({initialData}) => {
 
     const [data, setData] = useState([])
 
-    var [loadedRowsMap, loadingRowCount] = [{}, 0]
-
     const setNewData = item => {
         setData(data.map(el => {
             if (el.id === item.id) {
@@ -47,67 +45,31 @@ const Catalog = ({initialData}) => {
     }
 
     const loadMoreRows = ({startIndex, stopIndex}) => {
-        console.log(startIndex, stopIndex)
-        // const {loadedRowsMap, loadingRowCount} = this.state;
-        const increment = stopIndex - startIndex + 1;
-
-        for (var i = startIndex; i <= stopIndex; i++) {
-            loadedRowsMap[i] = STATUS_LOADING;
-        }
-
-        this.setState({
-            loadingRowCount: loadingRowCount + increment,
-        });
-
-        const timeoutId = setTimeout(() => {
-            // const {loadedRowCount, loadingRowCount} = this.state;
-
-            delete this._timeoutIdMap[timeoutId];
-
-            for (var i = startIndex; i <= stopIndex; i++) {
-                loadedRowsMap[i] = STATUS_LOADED;
-            }
-
-            this.setState({
-                loadingRowCount: loadingRowCount - increment,
-                loadedRowCount: loadedRowCount + increment,
-            });
-
-            promiseResolver();
-        }, 1000 + Math.round(Math.random() * 2000));
-
-        this._timeoutIdMap[timeoutId] = true;
-
-        let promiseResolver;
-
-        return new Promise(resolve => {
-            promiseResolver = resolve;
-        });
+        return new Promise();
     }
 
     return (
-        <div className={ style.catalog}>
+        <div className={style.catalog}>
             <h2 className="title title__h2 ">Фотобудки</h2>
             <Sort sortHandler={sortData}/>
 
             {data &&
-            // data.map(item => <CatalogItem key={item.id} setNewData={setNewData} data={item}/>) //80
-                <InfiniteLoader
-                    isRowLoaded={isRowLoaded}
-                    rowCount={data.length}
-                    loadMoreRows={loadMoreRows}>
-                    {({onRowsRendered, registerChild}) => (
-                        <List
-                            ref={registerChild}
-                            height={765 * data.length}
-                            onRowsRendered={onRowsRendered}
-                            rowCount={data.length}
-                            rowHeight={765}
-                            rowRenderer={rowRender}
-                            width={330}
-                        />
-                    )}
-                </InfiniteLoader>
+            <InfiniteLoader
+                isRowLoaded={isRowLoaded}
+                rowCount={data.length}
+                loadMoreRows={loadMoreRows}>
+                {({onRowsRendered, registerChild}) => (
+                    <List
+                        ref={registerChild}
+                        height={780 * data.length}
+                        onRowsRendered={onRowsRendered}
+                        rowCount={data.length}
+                        rowHeight={780}
+                        rowRenderer={rowRender}
+                        width={330}
+                    />
+                )}
+            </InfiniteLoader>
             }
         </div>
     )
